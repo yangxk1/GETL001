2a271222dea841ff7b92e579a6dff0c8c011455d
yxk485490 <yxk485490@alibaba-inc.com>
2025-12-04 14:16:51 +0800

    speed up

--- src/main/java/com/getl/converter/SG/TinkerPopToOG.java
+++ src/main/java/com/getl/converter/SG/TinkerPopToOG.java
@@ -41,24 +41,22 @@ public class TinkerPopToOG {
     }
 
     /**
-     * Adds the given {@link Graph} to the current {@link #dataset}.
+     * Adds the given {@link LPGGraph} to the current {@link #dataset}.
      *
      * @param graph The LPG graph to add.
      */
     public void addTinkerPopToOGDataset(@NonNull Graph graph) {
 
         // Adds statements for the vertices
-        Iterator<Vertex> vertices = graph.vertices();
-        while (vertices.hasNext()) {
-            Vertex vertex = vertices.next();
+        for (Iterator<Vertex> it = graph.vertices(); it.hasNext(); ) {
+            Vertex vertex = it.next();
             OGSimpleNode<?> sn = this.dataset.createOrObtainSimpleNode(vertex);
             this.addStatementsForVertexLabels(sn, vertex);
             this.addPropertyStatementsForElementProperties(sn, vertex);
         }
         // Add statements for the edges
-        Iterator<Edge> edges = graph.edges();
-        while (edges.hasNext()) {
-            Edge edge = edges.next();
+        for (Iterator<Edge> it = graph.edges(); it.hasNext(); ) {
+            Edge edge = it.next();
             OGRelationshipStatement r =
                     this.addOrObtainRelationshipStatementForEdge(edge);
             this.addPropertyStatementsForElementProperties(r, edge);
@@ -66,39 +64,9 @@ public class TinkerPopToOG {
     }
 
     // Adds property statements to the dataset for all properties of the given element (vertex/edge)
-    private void addPropertyStatementsForElementProperties(OGReifiableElement subElem, LPGElement elem) {
-        for (LPGProperty p : elem.getProperties()) {
-            for (Object value : p.values) {
-                OGValue<?> objValue = new OGValue<>(value);
-
-                Optional<LPGMappingConfiguration.ELMorPNMEntry<?>> entry = this.configuration.propertyNameMapping(p.name);
-                if (entry.isPresent()) {
-                    entry.get().unpack(new LPGMappingConfiguration.MappingEntryDelegate() {
-                        @Override
-                        public void handleIRI(IRI i) {
-                            OGPredicate<?> pred = dataset.createOrObtainSimpleNode(i);
-                            dataset.addStatementAndAddToDefaultGraph(new OGPropertyStatement(subElem, pred, objValue));
-                        }
-
-                        @Override
-                        public void handleString(String s) {
-                            OGPredicate<?> pred = new OGPredicateString(s);
-                            dataset.addStatementAndAddToDefaultGraph(new OGPropertyStatement(subElem, pred, objValue));
-                        }
-                    });
-                } else {
-                    OGPredicate<?> pred = new OGPredicateString(p.name);
-                    dataset.addStatementAndAddToDefaultGraph(new OGPropertyStatement(subElem, pred, objValue));
-                }
-            }
-        }
-    }
-
-    // Adds property statements to the dataset for all properties of the given element (vertex/edge)
     private void addPropertyStatementsForElementProperties(OGReifiableElement subElem, Element elem) {
-        Set<String> keys = elem.keys();
-        for (String key : keys) {
-            Property<Object> p = elem.property(key);
+        for (Iterator<? extends Property<Object>> it = elem.properties(); it.hasNext(); ) {
+            Property p = it.next();
             Object value = p.value();
             OGValue<?> objValue = new OGValue<>(value);
 
@@ -125,38 +93,9 @@ public class TinkerPopToOG {
     }
 
     // Adds statements to the 1G data set for the vertex labels
-    private void addStatementsForVertexLabels(OGSimpleNode<?> snSub, LPGVertex v) {
-        for (String label : v.getLabels()) {
-
-            Optional<LPGMappingConfiguration.NLMEntry<?>> entry = this.configuration.nodeLabelMapping(label);
-
-            if (entry.isPresent()) {
-                OGSimpleNodeIRI snPred = this.dataset.createOrObtainSimpleNode(entry.get().predicate);
-
-                entry.get().unpack(new LPGMappingConfiguration.MappingEntryDelegate() {
-                    @Override
-                    public void handleIRI(IRI i) {
-                        OGSimpleNodeIRI snObj = dataset.createOrObtainSimpleNode(i);
-                        dataset.addStatementAndAddToDefaultGraph(new OGRelationshipStatement(snSub, snPred, snObj));
-                    }
-
-                    @Override
-                    public void handleString(String s) {
-                        OGValue<String> valObj = new OGValue<>(s);
-                        dataset.addStatementAndAddToDefaultGraph(new OGPropertyStatement(snSub, snPred, valObj));
-                    }
-                });
-            } else {
-                // Use the default node label, add a property statement
-                OGSimpleNodeIRI snPred = this.dataset.createOrObtainSimpleNode(this.configuration.defaultNodeLabelPredicate);
-                dataset.addStatementAndAddToDefaultGraph(new OGPropertyStatement(snSub, snPred, new OGValue<>(label)));
-            }
-        }
-    }
-
-    // Adds statements to the 1G data set for the vertex labels
     private void addStatementsForVertexLabels(OGSimpleNode<?> snSub, Vertex v) {
         String label = v.label();
+
         Optional<LPGMappingConfiguration.NLMEntry<?>> entry = this.configuration.nodeLabelMapping(label);
 
         if (entry.isPresent()) {
@@ -183,50 +122,6 @@ public class TinkerPopToOG {
     }
 
     // Creates and returns a relationship statement from the given LPG edge
-    private OGRelationshipStatement addOrObtainRelationshipStatementForEdge(LPGEdge edge) {
-        OGSimpleNode<?> outNode = dataset.createOrObtainSimpleNode(edge.outVertex);
-        OGSimpleNode<?> inNode = dataset.createOrObtainSimpleNode(edge.inVertex);
-
-        Object edgeID = edge.getId();
-        // Try to obtain an existing relationship statement with this ID.
-        Optional<OGRelationshipStatement> optRel = dataset.getRelationshipStatementForLPGEdgeID(edgeID);
-        if (optRel.isPresent()) {
-            // If the relationship statement already in the data set has the same in and out node
-            // it is the same edge, return it.
-            if (optRel.get().subject == outNode && optRel.get().object == inNode) {
-                return optRel.get();
-            } else {
-                // There is a relationship statement in the data set with the same linked edge ID.
-                // but the in- or out- node does not match, create a new rel with a different unique ID in this case.
-                edgeID = String.format("%s-%s-%s", edgeID, edge.outVertex, edge.inVertex);
-            }
-        }
-        OGRelationshipStatement rel = new OGRelationshipStatement(
-                dataset.createOrObtainSimpleNode(edge.outVertex),
-                dataset.createOrObtainSimpleNode(edge.inVertex));
-        rel.edgeIDLPG = edgeID;
-
-        Optional<LPGMappingConfiguration.ELMorPNMEntry<?>> entry = this.configuration.edgeLabelMapping(edge.label);
-        if (entry.isPresent()) {
-            entry.get().unpack(new LPGMappingConfiguration.MappingEntryDelegate() {
-                @Override
-                public void handleIRI(IRI i) {
-                    rel.predicate = dataset.createOrObtainSimpleNode(i);
-                }
-
-                @Override
-                public void handleString(String s) {
-                    rel.predicate = new OGPredicateString(s);
-                }
-            });
-        } else {
-            rel.predicate = new OGPredicateString(edge.label);
-        }
-        dataset.addStatementAndAddToDefaultGraph(rel);
-        return rel;
-    }
-
-    // Creates and returns a relationship statement from the given LPG edge
     private OGRelationshipStatement addOrObtainRelationshipStatementForEdge(Edge edge) {
         OGSimpleNode<?> outNode = dataset.createOrObtainSimpleNode(edge.outVertex());
         OGSimpleNode<?> inNode = dataset.createOrObtainSimpleNode(edge.inVertex());
@@ -242,7 +137,7 @@ public class TinkerPopToOG {
             } else {
                 // There is a relationship statement in the data set with the same linked edge ID.
                 // but the in- or out- node does not match, create a new rel with a different unique ID in this case.
-                edgeID = String.format("%s-%s-%s", edgeID, edge.outVertex().id(), edge.inVertex().id());
+                edgeID = String.format("%s-%s-%s", edgeID, edge.outVertex(), edge.inVertex());
             }
         }
         OGRelationshipStatement rel = new OGRelationshipStatement(
--- src/main/java/com/getl/example/converter/TinkerPopMapperTest.java
+++ src/main/java/com/getl/example/converter/TinkerPopMapperTest.java
@@ -1,7 +1,9 @@
 package com.getl.example.converter;
 
+import com.getl.constant.CommonConstant;
 import com.getl.converter.ConverterUtils;
 import com.getl.example.Runnable;
+import com.getl.io.AsyncLPGParser;
 import com.getl.model.onegraph.dataset.OGDataset;
 import com.getl.util.GetlLogger;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
@@ -11,6 +13,7 @@ import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Test for direct TinkerPop Graph to SG conversion without LPG intermediate layer
@@ -45,17 +48,19 @@ public class TinkerPopMapperTest extends Runnable {
         System.out.println("【测试1】TinkerPop → SG 转换一致性");
         System.out.println("-".repeat(60));
 
-        Graph originalGraph = createSampleTinkerGraph();
+        Graph originalGraph = loadData();
+        System.out.println("加载完成的图数据");
+
+        // 旧方法：通过 LPG
+        long oldStart = System.nanoTime();
+        OGDataset datasetOld = ConverterUtils.buildSGFromTinkerPopGraphWithLPG(originalGraph);
+        long oldTime = System.nanoTime() - oldStart;
 
         // 新方法：直接转换
         long newStart = System.nanoTime();
         OGDataset datasetNew = ConverterUtils.buildSGFromTinkerPopGraph(originalGraph);
         long newTime = System.nanoTime() - newStart;
 
-        // 旧方法：通过 LPG
-        long oldStart = System.nanoTime();
-        OGDataset datasetOld = ConverterUtils.buildSGFromTinkerPopGraphWithLPG(originalGraph);
-        long oldTime = System.nanoTime() - oldStart;
 
         // 验证结果一致性
         boolean consistent = verifyOGDatasetConsistency(datasetNew, datasetOld);
@@ -136,7 +141,7 @@ public class TinkerPopMapperTest extends Runnable {
         System.out.println("【测试3】往返转换一致性验证");
         System.out.println("-".repeat(60));
 
-        Graph originalGraph = createSampleTinkerGraph();
+        Graph originalGraph = loadData();
         GraphTraversalSource gOrig = originalGraph.traversal();
         long origV = gOrig.V().count().next();
         long origE = gOrig.E().count().next();
@@ -324,5 +329,68 @@ public class TinkerPopMapperTest extends Runnable {
     @Override
     protected GetlLogger initLogger() {
         return new GetlLogger("TinkerPopMapperTest");
+    }
+
+    private Graph loadData() {
+        AsyncLPGParser asyncParser = new AsyncLPGParser(8);
+        String BASE_URL = CommonConstant.LPG_FILES_BASE_URL;
+        long begin = System.currentTimeMillis();
+        String BASE_URL_STATIC = BASE_URL + "static/";
+        String BASE_URL_DYNAMIC = BASE_URL + "dynamic/";
+
+        try {
+            // 异步加载所有顶点文件
+            CompletableFuture<Void> vertexFutures = CompletableFuture.allOf(
+                    asyncParser.asyncLoadVertex(BASE_URL_STATIC + "organisation_0_0.csv", "Organisation"),
+                    asyncParser.asyncLoadVertex(BASE_URL_STATIC + "place_0_0.csv", "Place"),
+                    asyncParser.asyncLoadVertex(BASE_URL_STATIC + "tag_0_0.csv", "Tag"),
+                    asyncParser.asyncLoadVertex(BASE_URL_STATIC + "tagclass_0_0.csv", "TagClass"),
+                    asyncParser.asyncLoadVertex(BASE_URL_DYNAMIC + "comment_0_0.csv", "Comment", "creationDate", AsyncLPGParser.MILLI),
+                    asyncParser.asyncLoadVertex(BASE_URL_DYNAMIC + "forum_0_0.csv", "Forum", "creationDate", AsyncLPGParser.MILLI),
+                    asyncParser.asyncLoadVertex(BASE_URL_DYNAMIC + "person_0_0.csv", "Person", "birthday", AsyncLPGParser.MILLI, "creationDate", AsyncLPGParser.MILLI),
+                    asyncParser.asyncLoadVertex(BASE_URL_DYNAMIC + "post_0_0.csv", "Post", "creationDate", AsyncLPGParser.MILLI, "length", AsyncLPGParser.INT),
+                    asyncParser.asyncLoadVertex(BASE_URL_DYNAMIC + "person_email_emailaddress_0_0.csv", "Person"),
+                    asyncParser.asyncLoadVertex(BASE_URL_DYNAMIC + "person_speaks_language_0_0.csv", "Person")
+            );
+
+            // 异步加载所有边文件
+            CompletableFuture<Void> edgeFutures = CompletableFuture.allOf(
+                    asyncParser.asyncLoadEdge(BASE_URL_STATIC + "organisation_isLocatedIn_place_0_0.csv", "organisation_isLocatedIn_place", "Organisation", "Place"),
+                    asyncParser.asyncLoadEdge(BASE_URL_STATIC + "place_isPartOf_place_0_0.csv", "place_isPartOf_place", "Place", "Place"),
+                    asyncParser.asyncLoadEdge(BASE_URL_STATIC + "tag_hasType_tagclass_0_0.csv", "tag_hasType_tagclass", "Tag", "TagClass"),
+                    asyncParser.asyncLoadEdge(BASE_URL_STATIC + "tagclass_isSubclassOf_tagclass_0_0.csv", "tagclass_isSubclassOf_tagclass", "TagClass", "TagClass"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "comment_hasCreator_person_0_0.csv", "comment_hasCreator_person", "Comment", "Person"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "comment_hasTag_tag_0_0.csv", "comment_hasTag_tag", "Comment", "Tag"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "comment_isLocatedIn_place_0_0.csv", "comment_isLocatedIn_place", "Comment", "Place"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "comment_replyOf_comment_0_0.csv", "comment_replyOf_comment", "Comment", "Comment"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "comment_replyOf_post_0_0.csv", "comment_replyOf_post", "Comment", "Post"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "forum_containerOf_post_0_0.csv", "forum_containerOf_post", "Forum", "Post"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "forum_hasMember_person_0_0.csv", "forum_hasMember_person", "Forum", "Person", "joinDate", AsyncLPGParser.MILLI),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "forum_hasModerator_person_0_0.csv", "forum_hasModerator_person", "Forum", "Person"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "forum_hasTag_tag_0_0.csv", "forum_hasTag_tag", "Forum", "Tag"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "person_hasInterest_tag_0_0.csv", "person_hasInterest_tag", "Person", "Tag"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "person_isLocatedIn_place_0_0.csv", "person_isLocatedIn_place", "Person", "Place"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "person_knows_person_0_0.csv", "person_knows_person", "Person", "Person", "creationDate", AsyncLPGParser.MILLI),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "person_likes_comment_0_0.csv", "person_likes_comment", "Person", "Comment", "creationDate", AsyncLPGParser.MILLI),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "person_likes_post_0_0.csv", "person_likes_post", "Person", "Post", "creationDate", AsyncLPGParser.MILLI),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "person_studyAt_organisation_0_0.csv", "person_studyAt_organisation", "Person", "Organisation", "classYear", AsyncLPGParser.INT),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "person_workAt_organisation_0_0.csv", "person_workAt_organisation", "Person", "Organisation", "workFrom", AsyncLPGParser.INT),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "post_hasCreator_person_0_0.csv", "post_hasCreator_person", "Post", "Person"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "post_hasTag_tag_0_0.csv", "post_hasTag_tag", "Post", "Tag"),
+                    asyncParser.asyncLoadEdge(BASE_URL_DYNAMIC + "post_isLocatedIn_place_0_0.csv", "post_isLocatedIn_place", "Post", "Place")
+            );
+
+            // 等待所有文件读取任务完成
+            CompletableFuture.allOf(vertexFutures, edgeFutures).join();
+
+            // 等待所有数据写入完成
+            asyncParser.awaitCompletion();
+            return asyncParser.getGraph();
+        } catch (Throwable throwable) {
+            throw new RuntimeException(throwable);
+        } finally {
+            // 关键修复：必须关闭 AsyncLPGParser 以终止后台线程
+            asyncParser.shutdown();
+        }
     }
 }
